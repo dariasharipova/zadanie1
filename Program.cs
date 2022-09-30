15460457f24d6f139a8197647e815f4f583eaee1
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2022-09-30 09:55:41 +0500

    добавлено первое дз

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("А.С. Пушкин");
+//Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("\nУ лукоморья дуб зелёный(отрывок из поэмы \"Руслан и Людмила\"");
+//Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("\nУ лукоморья дуб зелёный;\nЗлатая цепь на дубе том:\nИ днём и ночью кот учёный\nВсё ходит по цепи кругом");
