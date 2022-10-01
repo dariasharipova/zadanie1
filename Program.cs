22c0c019cb2ddb02fdf48b213ebccd1dc13405e9
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2022-10-01 13:40:38 +0500

    выполнено задание 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
-﻿//Console.BackgroundColor = ConsoleColor.Yellow;
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Сергей Есенин");
 Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("А.С. Пушкин");
-//Console.BackgroundColor = ConsoleColor.White;
-Console.ForegroundColor = ConsoleColor.Black;
-Console.WriteLine("\nУ лукоморья дуб зелёный(отрывок из поэмы \"Руслан и Людмила\"");
-//Console.BackgroundColor = ConsoleColor.Blue;
-Console.ForegroundColor = ConsoleColor.White;
-Console.WriteLine("\nУ лукоморья дуб зелёный;\nЗлатая цепь на дубе том:\nИ днём и ночью кот учёный\nВсё ходит по цепи кругом");
+Console.WriteLine("Что это такое?");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("В этот лес завороженный,\nПо пушинкам серебра,\nЯ с винтовкой заряженной\nНа охоту шел вчера.");
