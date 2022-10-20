f2df5a1187f7fb87a2d443ceb3d277a5768772eb
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2022-10-20 19:14:02 +0500

    возврат цвета в изначальное состояние

--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine("Что это такое?");
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("В этот лес завороженный,\nПо пушинкам серебра,\nЯ с винтовкой заряженной\nНа охоту шел вчера.");
+Console.ResetColor();
